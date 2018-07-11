a11b50417de776bf516ad4a622699e208e7ad635
Zach <zachmatuson@gmail.com>
2018-07-11 15:34:43 -0400

    Cleaning up leftover .js files

--- src/Rendering/Camera/OrthoCamera.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Camera_1 = require("./Camera");
-class OrthoCamera extends Camera_1.Camera {
-}
-exports.OrthoCamera = OrthoCamera;
-//# sourceMappingURL=OrthoCamera.js.map
