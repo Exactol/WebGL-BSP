42fb446c4482e080f792be48d35878f65feb61fa
Zach <zachmatuson@gmail.com>
2018-07-11 15:35:04 -0400

    Cleaning up leftover .js files

--- src/Rendering/Camera/PerspectiveCamera.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Camera_1 = require("./Camera");
-class PerspectiveCamera extends Camera_1.Camera {
-}
-exports.PerspectiveCamera = PerspectiveCamera;
-//# sourceMappingURL=PerspectiveCamera.js.map
